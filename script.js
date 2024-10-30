71d72cb9569245bb441243b17c7bde38f336ad97
Amir Mohammad Ahmadi <149728309+AmirAM03@users.noreply.github.com>
2024-10-30 17:04:40 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        window.scrollTo({
+            top: targetSection.offsetTop - 50,
+            behavior: 'smooth'
+        });
+    });
+});
